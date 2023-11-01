239942ee5dc2a66ee2a16a84c44bb7632bcbc078
XaviBarragan <xbarragan73@gmail.com>
2023-11-01 12:05:58 +0100

    Añadir ejercicio-14

--- src/ejercicio-14/index.js
+++ src/ejercicio-14/index.js
@@ -1,1 +1,10 @@
-// Escribe tu código aquí:
+// Ejercicio 14 - Verificar Número Entero
+
+// Dado un valor, utiliza el método Number.isInteger() para verificar si es un número entero. Luego, imprime el resultado para ambos ejemplos.
+const num1 = 33
+const num2 = 3.14
+
+console.log(Number.isInteger(num1))
+// Resultado: true
+console.log(Number.isInteger(num2))
+// Resultado: false
