40baa200731f5e264d449b75e555df4677d5b62c
XaviBarragan <xbarragan73@gmail.com>
2023-11-01 13:34:48 +0100

    Añadir ejercicio-23

--- src/ejercicio-23/index.js
+++ src/ejercicio-23/index.js
@@ -1,1 +1,11 @@
-// Escribe tu código aquí:
+// Ejercicio 23 - Copia Superficial de Subarray
+
+// Dado un array de números, utiliza el método slice() para crear una copia superficial de una porción del array. Luego, imprime el subarray resultante.
+const numeros = [1, 2, 3, 4, 5]
+const subArray = numeros.slice(2, 4)
+// slice selecciona desde la posicion 2 hasta la 4 sin incluir esta última
+// slice(p): selecciona todos los elementos desde la posicion p
+// slice(-p): selecciona las últimas p posiciones del Array
+
+console.log(subArray)
+// Resultado: [3, 4]
