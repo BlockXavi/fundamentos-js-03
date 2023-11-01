443464d7c73498005dc94af6d9991a63fec09185
XaviBarragan <xbarragan73@gmail.com>
2023-11-01 13:10:05 +0100

    Añadir ejercicio-20

--- src/ejercicio-20/index.js
+++ src/ejercicio-20/index.js
@@ -1,1 +1,10 @@
-// Escribe tu código aquí:
+// Ejercicio 20 - Eliminar Último Elemento
+
+// Dado un array de colores, utiliza el método pop() para eliminar el último color del array y mostrarlo en la consola. Luego, imprime el array resultante.
+const colores = ['rojo', 'verde', 'azul']
+const colorEliminado = colores.pop()
+
+console.log(colorEliminado)
+// Resultado: 'azul'
+console.log(colores)
+// Resultado: ['rojo', 'verde']
