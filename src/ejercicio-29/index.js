22d3c4fc12ef23818ad5abc6131e1736eec5e322
XaviBarragan <xbarragan73@gmail.com>
2023-11-01 14:22:34 +0100

    Añadir ejercicio-29

--- src/ejercicio-29/index.js
+++ src/ejercicio-29/index.js
@@ -1,1 +1,8 @@
-// Escribe tu código aquí:
+// Ejercicio 29 - Crear Nuevo Array con filter()
+
+// Dado un array de números, utiliza el método filter() para crear un nuevo array que contenga solo los números pares. Luego, imprime el nuevo array resultante.
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const numerosPares = numeros.filter((num) => num % 2 === 0)
+
+console.log(numerosPares)
+// Resultado: [2, 4, 6, 8, 10]
