966729267be21b1556d2e7ec77f937cd3c09a78b
XaviBarragan <xbarragan73@gmail.com>
2023-11-01 12:27:16 +0100

    Añadir ejercicio-17

--- src/ejercicio-17/index.js
+++ src/ejercicio-17/index.js
@@ -1,1 +1,9 @@
-// Escribe tu código aquí:
+// Ejercicio 17 - Redondear Arriba
+
+// Dado un número decimal, utiliza la función Math.ceil() para redondearlo hacia arriba al entero más cercano. Luego, imprime el resultado.
+const valor = 2.3
+const valorRedondeadoArriba = Math.ceil(valor)
+
+console.log(valorRedondeadoArriba)
+// Resultado: 3
+
