a0e3c180ab1d7c3761577bfe9b27579307fa731e
XaviBarragan <xbarragan73@gmail.com>
2023-11-01 11:56:28 +0100

    Añadir ejercicio-13

--- src/ejercicio-13/index.js
+++ src/ejercicio-13/index.js
@@ -1,1 +1,8 @@
-// Escribe tu código aquí:
+// Ejercicio 13 - Convertir a Flotante
+
+// Dada una cadena numérica, utiliza el método parseFloat() para convertirla en un número de punto flotante. Luego, imprime el resultado.
+const cadenaFloat = '3.14'
+const numeroFloat = parseFloat(cadenaFloat)
+
+console.log(numeroFloat)
+// Resultado: 3.14
