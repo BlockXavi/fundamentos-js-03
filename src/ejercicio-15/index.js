e6aeb76e16e538ce02af0bd2300760c8a08c43bd
XaviBarragan <xbarragan73@gmail.com>
2023-11-01 12:12:45 +0100

    Añadir ejercicio-15

--- src/ejercicio-15/index.js
+++ src/ejercicio-15/index.js
@@ -1,1 +1,10 @@
-// Escribe tu código aquí:
+// Ejercicio 15 - Redondear
+
+// Dado un número decimal, utiliza la función Math.round() para redondearlo al entero más cercano. Luego, imprime el resultado.
+const valor = 3.5
+const valorRedondeado = Math.round(valor)
+
+console.log(valorRedondeado)
+// Resultado: 4 (desde 3.5 ya redondea a 4)
+console.log(Number.isInteger(valorRedondeado))
+// Resultado: true
