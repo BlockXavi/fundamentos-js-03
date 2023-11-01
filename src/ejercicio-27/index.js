38a41e6147634b90846cb18c586c571cc8adf714
XaviBarragan <xbarragan73@gmail.com>
2023-11-01 14:10:37 +0100

    Añadir ejercicio-27

--- src/ejercicio-27/index.js
+++ src/ejercicio-27/index.js
@@ -1,1 +1,15 @@
-// Escribe tu código aquí:
+// Ejercicio 27 - Ejecutar para Cada Elemento
+
+// Dado un array de números, utiliza el método forEach() para ejecutar una función que multiplica cada número por 2. Luego, muestra los resultados en la consola.
+const numeros = [1, 2, 3, 4, 5]
+numeros.forEach((num) => {
+    console.log(num*=2)
+}
+)
+// Resultado:
+// 2
+// 4
+// 6
+// 8
+// 10
+
