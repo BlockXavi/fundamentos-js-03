ad020a40177b22efdad2e07fcd3b0750ea7eb1c9
XaviBarragan <xbarragan73@gmail.com>
2023-11-01 11:42:26 +0100

    Añadir ejercicio-12

--- src/ejercicio-12/index.js
+++ src/ejercicio-12/index.js
@@ -1,1 +1,8 @@
-// Escribe tu código aquí:
+// Ejercicio 12 - Convertir a Entero
+
+// Dada una cadena numérica, utiliza el método parseInt() para convertirla en un número entero. Luego, imprime el resultado.
+const cadenaNumerica = '33'
+const numeroInteger = parseInt(cadenaNumerica)
+
+console.log(numeroInteger)
+// Resultado: 33
