f2660574318864731bbd0437291c06eb153649e3
XaviBarragan <xbarragan73@gmail.com>
2023-11-01 14:02:29 +0100

    Añadir ejercicio-26

--- src/ejercicio-26/index.js
+++ src/ejercicio-26/index.js
@@ -1,1 +1,10 @@
-// Escribe tu código aquí:
+// Ejercicio 26 - Combinar Arrays
+
+// Dados dos arrays de números, utiliza el método concat() para combinarlos en un nuevo array. Luego, imprime el nuevo array resultante.
+const array1 = [1, 2, 3]
+const array2 = [4, 5, 6]
+const arrayCombinado = array1.concat(array2)
+
+console.log(arrayCombinado)
+// Resultado: [1, 2, 3, 4, 5, 6]
+
