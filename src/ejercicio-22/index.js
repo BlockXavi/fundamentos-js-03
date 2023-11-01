fc2d467e2dcab3472bbd74ae79e0c47c1dbd433f
XaviBarragan <xbarragan73@gmail.com>
2023-11-01 13:21:15 +0100

    Añadir ejercicio-22

--- src/ejercicio-22/index.js
+++ src/ejercicio-22/index.js
@@ -1,1 +1,10 @@
-// Escribe tu código aquí:
+// Ejercicio 22 - Eliminar Primer Elemento
+
+// Dado un array de animales, utiliza el método shift() para eliminar el primer animal del array y mostrarlo en la consola. Luego, imprime el array resultante.
+const animales = ['perro', 'gato', 'conejo']
+const animalEliminado = animales.shift()
+
+console.log(animalEliminado)
+// Resultado: 'perro'
+console.log(animales)
+// Resultado: ['gato', 'conejo']
