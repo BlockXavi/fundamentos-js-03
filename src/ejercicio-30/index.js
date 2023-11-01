af521d1b686565e9383b261b6e2c804031779a03
XaviBarragan <xbarragan73@gmail.com>
2023-11-01 14:30:02 +0100

    Añadir ejercicio-30

--- src/ejercicio-30/index.js
+++ src/ejercicio-30/index.js
@@ -1,1 +1,8 @@
-// Escribe tu código aquí:
+// Ejercicio 30 - Reducir a un Solo Valor
+
+// Dado un array de números, utiliza el método reduce() para sumar todos los números y obtener un valor total. Luego, imprime el valor total.
+const array = [1, 2, 3, 4, 5]
+const suma = array.reduce((total, num) => total+=num, 0) 
+
+console.log(suma)
+// Resultado: 15
