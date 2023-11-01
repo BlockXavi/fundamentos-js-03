186218418ba389e660ebac4f6e904b197ff9e1ef
XaviBarragan <xbarragan73@gmail.com>
2023-11-01 11:33:05 +0100

    Añadir ejercicio-11

--- src/ejercicio-11/index.js
+++ src/ejercicio-11/index.js
@@ -1,1 +1,8 @@
-// Escribe tu código aquí:
+// Ejercicio 11 - Formatear Decimales
+
+// Dado un número decimal, utiliza el método toFixed() para formatearlo con una cantidad específica de decimales. Luego, imprime el resultado.
+const valor = 3.14159
+const valorFormateado = valor.toFixed(2)
+
+console.log(valorFormateado)
+// Resultado: 3.14
