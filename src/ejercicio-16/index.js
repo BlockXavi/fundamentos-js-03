3371496564cff479dc49a9b109db9af29796bc2d
XaviBarragan <xbarragan73@gmail.com>
2023-11-01 12:20:26 +0100

    Añadir ejercicio-16

--- src/ejercicio-16/index.js
+++ src/ejercicio-16/index.js
@@ -1,1 +1,8 @@
-// Escribe tu código aquí:
+// Ejercicio 16 - Redondear Abajo
+
+// Dado un número decimal, utiliza la función Math.floor() para redondearlo hacia abajo al entero más cercano. Luego, imprime el resultado.
+const valor = 5.8
+const valorRedondeadoAbajo = Math.floor(valor)
+
+console.log(valorRedondeadoAbajo)
+// Resultado: 5
