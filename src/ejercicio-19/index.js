01d2fa040dfd061e8f0dfa6923b79963a6c0dbfa
XaviBarragan <xbarragan73@gmail.com>
2023-11-01 12:47:43 +0100

    Añadir ejercicio-19

--- src/ejercicio-19/index.js
+++ src/ejercicio-19/index.js
@@ -1,1 +1,8 @@
-// Escribe tu código aquí:
+// Ejercicio 19 - Agregar al Final
+
+// Dado un array de números, utiliza el método push() para agregar dos números al final del array. Luego, imprime el array resultante.
+const arrayNumeros = [1, 2, 3]
+arrayNumeros.push(4, 5)
+
+console.log(arrayNumeros)
+// Resultado: [1, 2, 3, 4, 5]
